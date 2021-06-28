a6488329144b573e37e07891bd1fb25d7355501d
AwesomeVariable <hyeonseo8312@gmail.com>
2021-06-28 23:50:22 +0900

    Create Social Login sys.

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,4 +1,4 @@
-import { authService } from "googlebase";
+import { authService, firebaseInstance } from "googlebase";
 import React, { useEffect, useState } from "react";
 
 const Auth = () => {
@@ -36,6 +36,20 @@ const Auth = () => {
     }
   };
   const toggleAccount = () => setNewAccount((prev) => !prev);
+  const onSocialClick = async (event) => {
+    const {
+      target: { name },
+    } = event;
+    let provider;
+    if (name === "google") {
+      provider = new firebaseInstance.auth.GoogleAuthProvider();
+    }
+    if (name === "github") {
+      provider = new firebaseInstance.auth.GithubAuthProvider();
+    }
+    const data = await authService.signInWithPopup(provider);
+    console.log(data);
+  };
 
   return (
     <div>
@@ -61,8 +75,12 @@ const Auth = () => {
       </form>
       <span onClick={toggleAccount}>{newAccount ? "Login" : "Sign Up"}</span>
       <div>
-        <button>Login with Google</button>
-        <button>Login with Github</button>
+        <button onClick={onSocialClick} name="google">
+          Login with Google
+        </button>
+        <button onClick={onSocialClick} name="github">
+          Login with Github
+        </button>
       </div>
     </div>
   );
